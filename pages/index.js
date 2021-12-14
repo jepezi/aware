e1d8a14fab6ac2da999e6172d240d4156821919c
jepezi <jepezi@gmail.com>
2021-12-14 18:09:25 +0800

    favicon

--- pages/index.js
+++ pages/index.js
@@ -42,6 +42,10 @@ export default function IndexPage({ postList }) {
     <div>
       <Head>
         <title>AWARE NFT</title>
+        <link key="1" rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+        <link key="2" rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+        <link key="3" rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+        <link key="4" rel="manifest" href="/site.webmanifest">
       </Head>
 
       <Banner />
